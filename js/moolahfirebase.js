d3017d94af12972d6b7d73e4c091fd7f162deb70
MaxWofford <max.speed.wofford@gmail.com>
2014-06-09 18:30:44 -0700

    Added temporary moolah form

---
+++ js/moolahfirebase.js
@@ -0,0 +1,12 @@
+  var fb = new Firebase("https://shibe-games.firebaseio.com/recommendations/moolah");
+var recommend = function (userEmail, userFeedback) {
+    alert("sending feedback"); /*Popup window*/
+    var userFeedback = document.getElementById("game-title").value;
+    var userEmail = document.getElementById("email").value;
+ 	fb.push({email: userEmail, feedback: userFeedback}); /*Pushes data to firebase*/
+ 	document.getElementById("instructions").innerHTML = "Recommendation recieved! You can recommend another here if you want, or head back to our main page for more awesome sales." ;
+    return false;
+}
+
+
+
