f1a9339e4fa2e5a0b5f44bbd611fd8a4d660cee0
MaxWofford <max.speed.wofford@gmail.com>
2014-06-19 14:33:52 -0700

    Added reddit username for summer sales

--- js/moolahfirebase.js
+++ js/moolahfirebase.js
@@ -1,13 +1,11 @@
-  var fb = new Firebase("https://shibe-games.firebaseio.com/recommendations/moolah");
-var recommend = function (userEmail, userFeedback) {
+  var fb = new Firebase("https://shibe-games.firebaseio.com/recommendations/moolah/summer-sale");
+var recommend = function (userEmail, userFeedback, userReddit) {
     alert("sending feedback"); /*Popup window*/
     document.getElementById("headBack").style.display = "inline";
     var userFeedback = document.getElementById("game-title").value;
+    var userReddit = document.getElementById("reddit").value;
     var userEmail = document.getElementById("email").value;
- 	fb.push({email: userEmail, feedback: userFeedback}); /*Pushes data to firebase*/
+ 	fb.push({reddit: userReddit, email: userEmail, feedback: userFeedback}); /*Pushes data to firebase*/
  	document.getElementById("instructions").innerHTML = "Recommendation recieved! You can recommend another here if you want, or head back to our main page for more awesome sales." ;
     return false;
 }
-
-
-
